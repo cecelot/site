666713aab93589dfa777dc237585ce53901e208e
Alaina <68250402+alaidriel@users.noreply.github.com>
2024-06-01 13:14:48 -0500

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
 import { remarkReadingTime } from "./src/readingTime";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://alainacn.dev",
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   markdown: {
     remarkRehype: {
       footnoteBackContent: "^",
