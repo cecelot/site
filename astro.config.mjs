527e54f3b89455121bfc0f158f47974453cdcf0e
Alaina <68250402+alaidriel@users.noreply.github.com>
2024-07-24 18:25:19 -0500

    feat: interactive tags filter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,21 +4,25 @@ import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
 import { remarkReadingTime } from "./src/readingTime";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://sydneyn.dev",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), react()],
   markdown: {
     shikiConfig: {
       themes: {
         light: "catppuccin-latte",
-        dark: "catppuccin-macchiato",
-      },
+        dark: "catppuccin-macchiato"
+      }
     },
     syntaxHighlight: "shiki",
     remarkRehype: {
-      footnoteBackContent: "^",
+      footnoteBackContent: "^"
     },
-    remarkPlugins: [[remarkToc, { ordered: true }], remarkReadingTime],
-  },
+    remarkPlugins: [[remarkToc, {
+      ordered: true
+    }], remarkReadingTime]
+  }
 });
---
+++ src/components/interactive/TagList.tsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import Fuse from "fuse.js";
+
+interface TagListProps {
+  tagNames: string[];
+  tags: Record<string, number>;
+}
+
+export default function TagList({ tagNames, tags }: TagListProps) {
+  const [query, setQuery] = useState("");
+  const [filtered, setFiltered] = useState<string[]>(tagNames);
+
+  useEffect(() => {
+    if (query) {
+      const fuse = new Fuse(
+        tagNames.map((tag) => ({ tag })),
+        {
+          keys: ["tag"],
+        }
+      );
+      const results = fuse.search(query);
+      setFiltered(results.map((result) => result.item.tag));
+    } else {
+      setFiltered(tagNames);
+    }
+  }, [query]);
+
+  return (
+    <div className="flex flex-col space-y-3">
+      <input
+        name="tag-filter"
+        placeholder="Filter tags..."
+        className="border-4 rounded-lg p-2 bg-mantle border-crust"
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <div>
+        <hr className="my-3" />
+      </div>
+      {filtered.length === 0 && <p>{"No tags matched that query :("}</p>}
+      {filtered
+        .sort((a, b) => a.localeCompare(b))
+        .map((tag) => (
+          <div className="flex flex-row space-x-3" key={tag}>
+            <a href={`/tags/${tag}`}>#{tag}</a>
+            <span className="text-subtext1">·</span>
+            <span className="text-subtext1">
+              {tags[tag]} post{tags[tag] > 1 ? "s" : ""}
+            </span>
+          </div>
+        ))}
+    </div>
+  );
+}
