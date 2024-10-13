8523b4c6a355e1cf08c97710f2ce1a29c0784f15
Sydney <68250402+cecelot@users.noreply.github.com>
2024-10-12 22:36:23 -0400

    feat: store images in R2

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly R2_ACCOUNT_ID: string;
+  readonly R2_ACCESS_KEY_ID: string;
+  readonly R2_SECRET_ACCESS_KEY: string;
+  readonly R2_BUCKET_NAME: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- src/image.ts
+++ src/image.ts
@@ -1,41 +1,85 @@
+import {
+  S3Client,
+  ListObjectsV2Command,
+  GetObjectCommand,
+} from "@aws-sdk/client-s3";
 import { format, parse } from "date-format-parse";
 import { baseUrl } from "../data/config.json";
 import type { Transform } from "./types";
 import ExifReader from "exifreader";
 
-const imageDir = "/img/gallery";
+const imageBase = "https://c.sydneyn.dev/gallery";
 
 export const transformImage = (name: string, transform: Transform) => {
   const format = transform.format ? transform.format : "auto";
-  return `${baseUrl}/cdn-cgi/image/width=${transform.width},height=${transform.height},quality=${transform.quality},format=${format}${imageDir}/${name}`;
+  return `${baseUrl}/cdn-cgi/image/width=${transform.width},height=${transform.height},quality=${transform.quality},format=${format}/${imageBase}/${name}`;
 };
 
-export const getAllImages = async () => {
-  // This only exists on the server, but we also import this module on the client, so
-  // use dynamic import for this.
-  const { readdirSync, readFileSync } = await import("fs");
+const S3 = new S3Client({
+  region: "auto",
+  endpoint: `https://${import.meta.env.R2_ACCOUNT_ID!!}.r2.cloudflarestorage.com`,
+  credentials: {
+    accessKeyId: import.meta.env.R2_ACCESS_KEY_ID!!,
+    secretAccessKey: import.meta.env.R2_SECRET_ACCESS_KEY!!,
+  },
+});
 
-  return readdirSync("public" + imageDir)
-    .filter(
-      (name) =>
-        name.endsWith(".png") || name.endsWith(".jpg") || name.endsWith(".jpeg")
+const getImageKeys = async () => {
+  const imageKeys = (
+    await S3.send(
+      new ListObjectsV2Command({
+        Bucket: import.meta.env.R2_BUCKET_NAME,
+      })
     )
-    .map((name) => {
-      const fullPath = `public/${imageDir}/${name}`;
-      const buf = readFileSync(fullPath);
-      const exifData = ExifReader.load(buf);
-      const date = format(
-        parse(
-          exifData["DateTime"]?.description ?? "1970:01:01 00:00:00",
-          "YYYY:MM:DD HH:mm:ss"
-        ),
-        "DD MMM YYYY HH:mm"
-      );
-      return {
-        name,
-        date,
-        width: exifData["Image Width"]?.value,
-        height: exifData["Image Height"]?.value,
-      };
-    });
+  )
+    .Contents!!.filter((object) => object.Key!!.startsWith("gallery"))
+    .map((object) => object.Key!!);
+  return imageKeys;
 };
+
+const getAllImages = async () => {
+  const images = [];
+  for (const key of await getImageKeys()) {
+    const name = key?.split("/").pop()!!;
+    images.push({
+      name,
+      blob: await (
+        await S3.send(
+          new GetObjectCommand({
+            Bucket: import.meta.env.R2_BUCKET_NAME,
+            Key: key,
+          })
+        )
+      ).Body!!.transformToByteArray(),
+    });
+  }
+  return images.map((image) => {
+    const buf = Buffer.from(image.blob);
+    const exifData = ExifReader.load(buf);
+    const date = format(
+      parse(
+        exifData["DateTime"]?.description ?? "1970:01:01 00:00:00",
+        "YYYY:MM:DD HH:mm:ss"
+      ),
+      "DD MMM YYYY HH:mm"
+    );
+    return {
+      name: image.name,
+      date,
+      width: exifData["Image Width"]?.value,
+      height: exifData["Image Height"]?.value,
+    };
+  });
+};
+
+export function useMemo<T>(fn: () => Promise<T>) {
+  let value: T | undefined;
+  return async () => {
+    if (value === undefined) {
+      value = await fn();
+    }
+    return value;
+  };
+}
+
+export const allImages = useMemo(getAllImages);
